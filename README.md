9a93e5a49cbcf5f5fa47b3f374a3efaa255d0ffe
Jon Hemstreet <jhemstreet@arrow.com>
2015-07-22 10:44:14 -0600

    updates

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+*Work in progress*
+
 How to run
 ---
  
--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-var _  = require('lodash' ),
-    xj = require("xls-to-json");
+var _       = require( 'lodash' ),
+    xj      = require( "xls-to-json" ),
+    curl    = require('curlrequest');
 
 var obj = {
 
-    data: null,
+    data : null,
 
-    init: function(config) {
+    init : function ( config ) {
 
         var config = _.assign( config, {
-            "test": true
-        });
+            "test" : true
+        } );
 
-        xj({
-            input: "config/site.xls",  // input xls
-            output: "output/site.json", // output json
-            sheet: "Manufacture Pages"  // specific sheetname
-        }, function(err, result) {
+        xj( {
+            input : "config/site.xls",  // input xls
+            output : "output/site.json", // output json
+            sheet : "Manufacture Pages"  // specific sheetname
+        }, function ( err, result ) {
 
-            if(err) {
-                console.error(err);
+            if ( err ) {
+                console.error( err );
             } else {
 
-                _.forEach(result, function(data, key) {
+                _.forEach( result, function ( data, key ) {
 
-                    console.log(data["Supplier Name"], data["Description"]);
-                });
+                    console.log( data[ "Supplier Name" ], data[ "Description" ] );
+
+                    // Make request to sitecore
+                    curl.request({ url: 'http://eample.com', pretend: true }, function (err, stdout, meta) {
+                        console.log('%s %s', meta.cmd, meta.args.join(' '));
+                    });
+
+                } );
             }
 
-        }.bind(this));
+        }.bind( this ) );
 
     }
 };
 
-obj.init({});
+obj.init( {} );
