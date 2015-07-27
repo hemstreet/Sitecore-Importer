7e4a2e4b302a1813d4cccdda6ceee00e1653da52
Jon Hemstreet <jhemstreet@arrow.com>
2015-07-27 11:25:45 -0600

    updated path locations, updating tests

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var Sitecore = require('./lib/sitecore.js');
 var sitecore = new Sitecore();
 
 // Home directory
-//sitecore.readItem( '{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}' );
+sitecore.readItem( '{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}' );
 
 // Web Template
 //sitecore.readItem( '{AB86861A-6030-46C5-B394-E8F99E8B87DB}' );
@@ -37,12 +37,12 @@ var sitecore = new Sitecore();
 //);
 
  //Import Manufacture Pages sheet from import/spreadsheets/site.xls
-sitecore.updateFromSpreadSheet({
-        path: 'test.xls',
-        sheetName: 'Manufacture Pages',
-        destinationPath: '/sitecore/content/Hemstreet/Small Import'
-    }
-);
+//sitecore.updateFromSpreadSheet({
+//        path: 'test.xls',
+//        sheetName: 'Manufacture Pages',
+//        destinationPath: '/sitecore/content/Hemstreet/Small Import'
+//    }
+//);
 
 //sitecore.updateItemById('{5B075E7C-7430-49B2-8DFC-9A55A93CBBED}', {
 //    'ManufacturerName': 'Dummy Name',
